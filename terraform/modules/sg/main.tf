f70b20996a3286c1378b2aa7b27457eae8f2fa8c
Ezekiel Umesi <obusorezekiel@gmail.com>
2024-12-01 14:04:49 +0100

    roles

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -67,6 +67,13 @@ resource "aws_security_group" "ansible_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 
   # Outbound internet access
   egress {
